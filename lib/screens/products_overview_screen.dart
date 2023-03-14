064ae5986135801f38c6dac61ed3cd00ef11616a
AlexMMorar <alex93morar@gmail.com>
2023-03-14 17:35:56 +0200

    fetch items at start. At loading spinner

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -23,6 +23,7 @@ class ProductsOverviewScreen extends StatefulWidget {
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   var _showOnlyFavorites = false;
   var _isInit = false;
+  var _isLoading = false;
 
   @override
   void initState() {
@@ -36,7 +37,12 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   @override
   void didChangeDependencies() {
     if (!_isInit) {
-      Provider.of<Products>(context).fetchAndSetProducts();
+      setState(() {
+        _isLoading = true;
+      });
+      Provider.of<Products>(context).fetchAndSetProducts().then((_) {
+        _isLoading = false;
+      });
       _isInit = true;
     }
     super.didChangeDependencies();
@@ -91,6 +97,10 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
           ],
         ),
         drawer: AppDrawer(),
-        body: ProductsGrid(_showOnlyFavorites));
+        body: _isLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : ProductsGrid(_showOnlyFavorites));
   }
 }
